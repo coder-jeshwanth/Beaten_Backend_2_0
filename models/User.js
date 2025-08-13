daa719f56074777135b98f9573128c7ebebf4095
M Karthikeyan <amariyappan.karthikeyan@gmail.com>
2025-08-13 13:52:44 +0530

    Final Changes

--- models/User.js
+++ models/User.js
@@ -13,11 +13,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Email is required"],
       unique: true,
-      lowercase: true,
-      match: [
-        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-        "Please enter a valid email",
-      ],
+      lowercase: true
     },
     password: {
       type: String,
