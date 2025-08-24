d1b830dd96e411bc411c6091a04f259798156579
coder-jeshwanth <24mcaa32@kristujayanti.com>
2025-08-24 16:56:23 +0530

    cors updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://beaten-customer-panel.onrender.com","https://beaten-admin.onrender.com", "http://localhost:3001", "https://beaten1-1.onrender.com","https://beaten1-2.onrender.com", "*"],
+    origin: ["https://beaten-admin-zu8d.onrender.com","http://localhost:3000","https://beaten-customer-panel.onrender.com","https://beaten-admin.onrender.com", "http://localhost:3001", "https://beaten1-1.onrender.com","https://beaten1-2.onrender.com", "*"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
